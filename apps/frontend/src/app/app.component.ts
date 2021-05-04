9a1c17b7d8425d90fe92b985848a6bf233965d74
Jhorman Rus <jhormantito@upeu.edu.pe>
2021-05-04 14:03:56 -0500

    [frontend][add] auth0

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'edulingo-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  
+  constructor(@Inject(DOCUMENT) public document: Document, public sAuth0: AuthService) { }
 }
--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AuthModule.forRoot({
+      domain: 'wanna.auth0.com',
+      clientId: 'wjEIF235s4K6W5XHeq7BCTWIDAwI4bcq'
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
