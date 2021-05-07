c57b5553d0d4762de624ad84f010e62a1b22a555
Jhorman Rus <jhormantito@upeu.edu.pe>
2021-05-07 01:18:55 -0500

    [backend][add] swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,14 +5,31 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  //* Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Edulingo')
+    .setDescription('Edulingo API description')
+    .setVersion('1.0')
+    .build();
+  const customOptions: SwaggerCustomOptions = {
+    explorer: true,
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Edulingo API Docs',
+  };
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, customOptions);
+  
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
