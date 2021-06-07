73f7738e8d867a1512875e710f4c8fb5550f681a
Jhorman Rus <jhormanrus@gmail.com>
2021-06-06 22:40:21 -0500

    [frontend][new] disabled sessions and units

--- apps/frontend/src/app/pages/dash/components/learn/learn.component.ts
+++ apps/frontend/src/app/pages/dash/components/learn/learn.component.ts
@@ -2,6 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { Component, OnDestroy, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
+import { Session } from 'apps/frontend/src/app/models/session';
 import { environment } from 'apps/frontend/src/environments/environment';
 import { Subscription } from 'rxjs';
 import { Unit } from '../../../../models/unit';
@@ -26,7 +27,9 @@ export class LearnComponent implements OnInit, OnDestroy {
   ) {
     this.unitService.getUnits().subscribe(res => {
       for(let unit of res) {
-        for(let section of unit.section) {
+        unit.section.forEach((section, index) => {
+          let prevsection = unit.section[index - 1]
+          section.active = !(prevsection? (prevsection.session.filter(v => v.progress[0]?.status === 1).length < prevsection.session.length? true : false) : false)
           for(let session of section.session) {
             if (session.progress.length !== 0 && session.progress[0].status === 1) {
               session.class = 'bg-warning'
@@ -34,7 +37,7 @@ export class LearnComponent implements OnInit, OnDestroy {
               session.class = this.getRandomBg()
             }
           }
-        }
+        })
       }
       this.unidades = res;
     });
